a665a4273f52c902db046ffb543632bcd9e470e0
Subair Appakkan <sappakkan@national.aaa.com>
2020-05-11 13:33:42 +0530

    resolve commit

---
+++ EvolveGT-iOS/Features/Login/Forgot Password/PwdResetAckController.swift
@@ -0,0 +1,20 @@
+//
+//  PwdResetAckController.swift
+//  EvolveGT-iOS
+//
+//  Created by Subair Ariyil on 11/05/20.
+//  Copyright © 2020 YaraTech. All rights reserved.
+//
+
+import UIKit
+
+class PwdResetAckController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
