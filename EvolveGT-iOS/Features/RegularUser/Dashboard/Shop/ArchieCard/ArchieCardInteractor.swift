85fd5d8493a44601f809e351df69295e09328b51
Subair Appakkan <sappakkan@national.aaa.com>
2020-05-27 12:44:03 +0530

    Shop Archie card2

---
+++ EvolveGT-iOS/Features/RegularUser/Dashboard/Shop/ArchieCard/ArchieCardInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  ArchieCardInteractor.swift
+//  EvolveGT-iOS
+//
+//  Created by Subair Ariyil on 26/05/20.
+//  Copyright © 2020 YaraTech. All rights reserved.
+//
+
+import Foundation
---
+++ EvolveGT-iOS/Features/RegularUser/Dashboard/Shop/ArchieCard/ArchieCardViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ArchieCardViewController.swift
+//  EvolveGT-iOS
+//
+//  Created by Subair Ariyil on 26/05/20.
+//  Copyright © 2020 YaraTech. All rights reserved.
+//
+
+import UIKit
+
+class ArchieCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    
+
+}
