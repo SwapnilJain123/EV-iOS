32d875f6bb5c11bd653ecd4efacff69efd27d1f7
Sonali Nagde <sonalideepak2409@gmail.com>
2025-02-22 12:17:06 +0530

    Notification list UI designed and API integration

---
+++ EvolveGT-iOS/Features/RegularUser/Dashboard/Home/NotificationViewController.swift
@@ -0,0 +1,82 @@
+//
+//  NotificationViewController.swift
+//  EvolveGT-iOS
+//
+//  Created by Sonali Nagde on 07/02/25.
+//  Copyright © 2025 YaraTech. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+    
+    @IBOutlet weak var notificationTableView: UITableView!
+    @IBOutlet weak var noDataFoundLable: UILabel!
+
+    let interactor = HomeDataInteractor()
+    var notificaitonList: NotificationListResponse?
+    var notifications: [NotificationItem] = []
+    var currentPage = 1
+    var isLoading = false
+    var totalPages = 2
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Notifications"
+        self.noDataFoundLable.isHidden = true
+        if !isLoading {
+            isLoading = true
+            interactor.getNotificationList(page: currentPage)
+        }
+        interactor.notificationDelegate = self
+    }
+
+}
+
+extension NotificationViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.notifications.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let notificationCell = tableView.dequeueReusableCell(withIdentifier: "NotificationTableViewCell", for: indexPath) as! NotificationTableViewCell
+        
+        notificationCell.setData(notificationObject: self.notifications[indexPath.row])
+        return notificationCell
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let frameHeight = scrollView.frame.size.height
+
+        if offsetY > contentHeight - frameHeight - 100 { // Load more when 100px before bottom
+            if !isLoading && currentPage <= totalPages {
+                interactor.getNotificationList(page: currentPage)
+                currentPage += 1
+            }
+        }
+    }
+}
+
+extension NotificationViewController: NotificationDelegate {
+    func didFailToGetNotificaitonData() {
+        self.isLoading = false
+    }
+    
+    func didGetNotificaitonData(notificationData : NotificationListResponse?)
+    {
+        self.isLoading = false
+
+        if let notificationObject = notificationData?.results, !notificationObject.isEmpty {
+            self.noDataFoundLable.isHidden = true
+            self.notifications.append(contentsOf: notificationObject)
+        } else {
+            self.noDataFoundLable.isHidden = false
+        }
+        self.notificaitonList = notificationData
+        self.totalPages = notificationData?.pages ?? 0
+        self.notificationTableView.reloadData()
+    }
+}
+ 
