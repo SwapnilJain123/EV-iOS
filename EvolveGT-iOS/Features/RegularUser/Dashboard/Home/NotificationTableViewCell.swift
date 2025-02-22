65d8a444c2379bdc261ad7852fd78093f4054a2e
Sonali Nagde <sonalideepak2409@gmail.com>
2025-02-22 12:15:30 +0530

    Notification list UI designed and API integration

---
+++ EvolveGT-iOS/Features/RegularUser/Dashboard/Home/NotificationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationTableViewCell.swift
+//  EvolveGT-iOS
+//
+//  Created by Sonali Nagde on 07/02/25.
+//  Copyright © 2025 YaraTech. All rights reserved.
+//
+
+import UIKit
+
+class NotificationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
